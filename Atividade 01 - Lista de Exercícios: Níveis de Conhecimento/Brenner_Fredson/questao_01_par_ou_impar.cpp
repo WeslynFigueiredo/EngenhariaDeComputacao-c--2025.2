1ccdb9f208e199e0c224811a22f18f2afcf55e71
espadachinangolano1-jpg <espadachinangolano1@gmail.com>
2025-09-10 20:21:55 -0300

    Update questao_01_par_ou_impar.cpp
    fizemos esse código no site programiz....

--- Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Brenner_Fredson/questao_01_par_ou_impar.cpp
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Brenner_Fredson/questao_01_par_ou_impar.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int numero;
+    
+    cout << "manda pai:";
+    cin >> numero;
+
+    if (numero % 2 == 0) {
+        cout << "O numero " << numero << " é par." << endl;
+    } else { 
+        cout << "O numero " << numero << " é impar" << endl;
+    }
+    return 0;
+}

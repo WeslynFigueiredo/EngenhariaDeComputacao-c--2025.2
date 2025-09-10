79d039e3debcd990f463694680ebfbc69c266825
Leonard <dimiranddaleonarddo@gmail.com>
2025-09-10 20:52:15 -0300

    Add files via upload

---
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/leonardogomes/exercicio2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int idade;
+	
+cout << "Quantos anos voce tem?: ";
+cin >> idade;
+
+if (idade > 18){
+	cout << "Voce tem mais de 18 anos";
+} else {
+	cout << "Voce tem menos de 18 anos";
+}
+
+return 0;
+
+}

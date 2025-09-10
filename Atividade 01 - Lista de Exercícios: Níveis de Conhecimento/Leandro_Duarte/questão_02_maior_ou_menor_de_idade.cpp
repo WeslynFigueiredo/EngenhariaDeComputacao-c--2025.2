4efe9d94f42f22b12733278ef1ac7daed9dd728a
Mob <duartemarquesl68@gmail.com>
2025-09-10 20:20:52 -0300

    Update questão_02_maior_ou_menor_de_idade.cpp

--- Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Leandro_Duarte/questão_02_maior_ou_menor_de_idade.cpp
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Leandro_Duarte/questão_02_maior_ou_menor_de_idade.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+
+    cout << "Digite Sua Idade: ";
+    cin >> numero;
+
+    if (numero >= 18) {
+        cout << "Você é maior de idade" << endl;
+    } else {
+        cout << "Você é menor de idade" << endl;
+    }
+
+    return 0;
+}
 

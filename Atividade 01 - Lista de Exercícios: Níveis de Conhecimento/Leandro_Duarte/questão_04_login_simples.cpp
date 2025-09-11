da347d5f6ca2c2c1fb3cfd87a3fe80a1e8eff98e
Mob <duartemarquesl68@gmail.com>
2025-09-10 21:41:01 -0300

    questão_04_login_simples.cpp

--- Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Leandro_Duarte/questão_04_login_simples.cpp
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Leandro_Duarte/questão_04_login_simples.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string login; 
+    int senha;
+    
+    cout << "Digite seu usuario: "; cin >> login;
+    cout << "digite sua senha: "; cin >> senha;
+    
+    if (login == "admin" && senha == 1234){
+        
+        cout << "Login bem-sucedido!" << endl;
+    }else {
+        cout << "Usuário ou senha incorretos" << endl;
+    }
+    return 0;
+}
 

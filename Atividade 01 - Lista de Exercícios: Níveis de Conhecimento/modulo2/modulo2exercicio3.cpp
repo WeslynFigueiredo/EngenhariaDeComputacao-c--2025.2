8761e390b041b0374c3967080126d1a0aa2838af
Leonard <dimiranddaleonarddo@gmail.com>
2025-09-17 19:36:03 -0300

    Add files via upload

---
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/modulo2/modulo2exercicio3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+
+int i = 1;
+
+cout << "Contando numeros pares do 1 ate 20: ";
+
+while (i <= 20) {
+	
+	if (i % 2 == 0){
+		
+		cout << i << endl; 
+	}
+	
+	i++;
+}
+
+
+return 0;
+
+}

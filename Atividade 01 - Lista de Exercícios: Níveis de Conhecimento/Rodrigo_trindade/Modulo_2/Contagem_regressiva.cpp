264a42252e3fa6f95b36c81bfa02b473829c4167
Rodrigo L. <138634726+RloanBRG@users.noreply.github.com>
2025-09-17 22:22:40 -0300

    Create Contagem_regressiva.cpp

---
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Rodrigo_trindade/Modulo_2/Contagem_regressiva.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int number = 1;
+	
+	cout << "Contagem Regresiva: \n";
+	
+	while(number <= 100){
+		cout << number << endl;
+		number++;
+	}
+	
+	return 0;
+}

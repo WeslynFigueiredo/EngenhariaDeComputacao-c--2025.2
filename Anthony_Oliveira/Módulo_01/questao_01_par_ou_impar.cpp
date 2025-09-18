023014b8476269d29ec288945837a436d578b713
Anthony <167636115+To-ony@users.noreply.github.com>
2025-09-17 21:04:32 -0300

    Update questao_01_par_ou_impar.cpp

--- Anthony_Oliveira/Módulo_01/questao_01_par_ou_impar.cpp
+++ Anthony_Oliveira/Módulo_01/questao_01_par_ou_impar.cpp
@@ -2,32 +2,13 @@
 using namespace std;
 
 int main(){
-	int num_user;
-	int num_check;
-	int sair = 0;
-	while(true){
-		cout << "Digite um número inteiro" << endl;
-		cin >> num_user;
-		num_check = num_user;
-		if (num_check % 2 == 0) {
-			cout << "O número " << num_user << " é par" << endl << endl;
-			cout << "Deseja sair do programa?" << endl << "Digite 1 se sim"	<< endl;
-			cin >> sair;
-			cout << endl;
-			if (sair == 1) {
-				cout << "Obrigado por usar o programa!" << endl;
-				break;
-			}
-		} 
-		if (num_check % 2 != 0){
-			cout << "O número " << num_user << " é ímpar" << endl << endl;
-			cout << "Deseja sair do programa?" << endl << "Digite 1 se sim"	<< endl;
-			cin >> sair;
-			cout << endl;
-			if (sair == 1) {
-				cout << "Obrigado por usar o programa!" << endl;
-				break;	
-			}
-		}
+	int num;
+	cout << "Digite um número inteiro" << endl;
+	cin >> num;
+	if (num % 2 == 0) {
+		cout << "O número " << num << " é par" << endl << endl;
+	} else {
+		cout << "O número " << num << " é ímpar" << endl << endl;	
 	}
+	return 0;
 }

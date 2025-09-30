6d49cc809b1cd9dfd0bf8789dd2f3dc66973cfc5
Robson Luan <robsonluanmb@gmail.com>
2025-09-30 13:37:22 -0300

    Create questão 8. pares ate vinte.cpp

---
+++ Atividade 01 - Lista de Exercícios: Níveis de Conhecimento/Robson Luan/questão 8. pares ate vinte.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int i = 1;
+
+	cout << "  numeros pares de 1 a 20" << endl;
+	
+	while (i <= 20){
+		i++;
+	if (i % 2 == 0){
+		cout << i << endl;
+	}	
+}
+	cout << "sao pares" << endl;
+	return 0;
+}
